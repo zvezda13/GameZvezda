4ea9a9b2400f5e34965d0de60b92cc8763fb1578
zvezda13 <zizzzo13@gmail.com>
2018-10-04 11:51:50 +0300

    opraven cikyl

--- game/Program.cs
+++ game/Program.cs
@@ -14,90 +14,42 @@ namespace game
         {
             Console.WriteLine("Press arrows or Esc to exit");
             Program zvezda=new Program();
-                       
-            
-            while (zvezda.kb.Key != ConsoleKey.Escape)
-            {
+           
                zvezda.GetKey();
-            }
+          
            
         }
+         // ConsoleKeyInfo kb = Console.ReadKey();   //hvashta push-natite buttoni ot keybord-a 
 
-     
-        ConsoleKeyInfo kb = Console.ReadKey(); //hvashta push-natite buttoni ot keybord-a 
+
+        ConsoleKeyInfo kb;
         public void GetKey()
 
         {
-            switch (kb.Key)
+            do
             {
-                case (ConsoleKey.LeftArrow):
-                    Console.WriteLine("Left Arrow pressed");
-                    break;
-                case (ConsoleKey.RightArrow):
-                    Console.WriteLine("Right Arrow pressed");
-                    break;
-                default:
-                    Console.WriteLine("Neprovereno kopche e natisnato");
-                    break;
-            }
+                kb = Console.ReadKey();
+                switch (kb.Key)
+                {
+                    case (ConsoleKey.LeftArrow):
+                        Console.WriteLine("Left Arrow pressed");
+                        break;
+                    case (ConsoleKey.RightArrow):
+                        Console.WriteLine("Right Arrow pressed");
+                        break;
+                    default:
+                        Console.WriteLine("Neprovereno kopche e natisnato");
+                        break;
+                }
+
+                
+            } while (kb.Key != ConsoleKey.Escape);
         }
 
-        /*
-         *  //foreach (kb.Key in )
-            //    Console.WriteLine("Neprovereno kopche e natisnato");
-
-         *  switch(kb)
-            {
-                case (ConsoleKey.LeftArrow):
-            }
-            if (kb.Key == ConsoleKey.LeftArrow)
-                Console.WriteLine("Left Arrow pressed");
-            else
-                Console.WriteLine("Right Arrow pressed");
-        internal enum KeyCode : int
-               {
-                   /// <summary>
-                   /// The left arrow key.
-                   /// </summary>
-                   Left = 0x25,
-
-                   /// <summary>
-                   /// The right arrow key.
-                   /// </summary>
-                   Right,
-               }
-       
-
-
-       internal static class NativeKeyboard
-       {
-
-           private const int KeyPressed = 0x8000;
-
-
-           public static bool IsKeyDown(KeyCode key)
-           {
-               return (GetKeyState((int)key) & KeyPressed) != 0;
-
-           }
-
-           [System.Runtime.InteropServices.DllImport("user32.dll")]
-           private static extern short GetKeyState(int key);
-       }
-
-        /*
-      public static void IsLeft()
-      {
-          if  KeyCode= 0x25
-              Console.WriteLine("Natisna na lqvo.");
-          //return (GetKeyState((int)key) & KeyPressed) != 0;
-
-
-      }
-      */
-        // static ConsoleKeyInfo key = new ConsoleKeyInfo();
 
     }
 
 
 }
+
+
