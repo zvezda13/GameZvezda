76f189726b3a4b3fa7751ec87aaa21182fee10ff
zvezda13 <zizzzo13@gmail.com>
2018-10-11 15:35:25 +0300

    printnah

--- game/Program.cs
+++ game/Program.cs
@@ -12,10 +12,10 @@ namespace game
         static ConsoleKeyInfo kb;
         static int x = 0;
         static int y = 0;
-        static int height = 5;
-        static int weight = 10;
+        static int height = 4;
+        static int weight = 5;
         static char he = 'O';
-      
+
 
         public static void GetInput()
         {
@@ -28,7 +28,7 @@ namespace game
             {
                 x -= 1;
             }
-           if (kb.Key.Equals(ConsoleKey.RightArrow))
+            if (kb.Key.Equals(ConsoleKey.RightArrow))
             {
                 x += 1;
             }
@@ -37,42 +37,40 @@ namespace game
         {
             Console.Clear();
             Console.SetCursorPosition(x, y);
-            Console.Write(he);
+           // Console.Write(he);
+            char[,] arr = new char[6, 4]
+
+             {
+                 {'-','-','-','-' },
+                 {'|',' ',' ','|' },
+                 {'|',' ',' ','|' },
+                 {'|',' ',' ','|' },
+                 {'|',' ',' ','|' },
+                 {'-','-','-','-' }
+
+            };
+            int rowLength = arr.GetLength(0);
+            int colLength = arr.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(string.Format("{0} ", arr[i, j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+          
         }
 
-        public static void DrawBorder()
-        {
-          
-            for (int x = 0; x < weight; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    if (x == 0 || x == weight - 1 )
-                    {
-                        Console.Write("-");
-                    }
-                    else if(y == 0 || y == height-1)
-                     {
-                       Console.Write("|");
-                     }
-                   
-            }
-               
-            Console.WriteLine();
-            }
-        }
-        public static bool IsInsideRectangle( )
-        {
-            return y == 0;//|| y = height;
-        }
         static void Main(string[] args)
         {
           while (kb.Key != ConsoleKey.Escape)
             {
-                DrawBorder();
+                Draw();
                 GetInput();
                 Update();
-                Draw ();
+               
             }
         }
 
